b0748eacfa02dc4ad255d0351e7fe558ac0f8345
Shaikhaalammari <66787565+Shaikhaalammari@users.noreply.github.com>
2020-09-16 00:16:46 +0300

    Sign in & Sign up methods , config , user model passport

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcrypt");
+//db
+const { User } = require("../db/models");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../config/keys");
+
+exports.signup = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log("exports.signup -> hashedPassword", hashedPassword);
+    req.body.password = hashedPassword;
+    const newUser = await User.create(req.body);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  const { user } = req;
+  const payload = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+
+    expires: Date.now() + parseInt(JWT_EXPIRATION_MS), // the token will expire 15 minutes from when it's generated
+  };
+  const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+  res.json({ token });
+};
---
+++ middleware/passport.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const JWTStrategy = require("passport-jwt").Strategy;
+const { fromAuthHeaderAsBearerToken } = require("passport-jwt").ExtractJwt;
+const { JWT_SECRET } = require("../config/keys");
+const User = require("../db/models/User");
+
+exports.localStrategy = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({
+      where: { username },
+    });
+
+    const passwordsMatch = user
+      ? await bcrypt.compare(password, user.password)
+      : false;
+
+    if (passwordsMatch) {
+      return done(null, user);
+    }
+
+    return done(null, false);
+  } catch (error) {
+    return done(error);
+  }
+});
+
+exports.jwtStrategy = new JWTStrategy(
+  { jwtFromRequest: fromAuthHeaderAsBearerToken(), secretOrKey: JWT_SECRET },
+  async (jwtPayload, done) => {
+    if (Date.now() > jwtPayload.expires) {
+      return done(null, false); // this will throw a 401
+    }
+    try {
+      const user = await User.findByPk(jwtPayload.id);
+      done(null, user); // if there is no user, this will throw a 401
+    } catch (error) {
+      done(error);
+    }
+  }
+);
