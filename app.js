02bc69ca98bf4d7ebd6011a9ed196e29f625db73
FN Otaibi <nsayemlil@hotmail.com>
2020-09-17 09:43:38 +0300

    added routes ,db and setup

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+//create express instance
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+//db
+const db = require("./db");
+const { Message } = require("./db/models");
+
+//routes
+const messageRoutes = require("./routes/messages");
+
+// routers
+app.use("/messages", messageRoutes);
 
 const run = async () => {
   try {
-    await db.authenticate();
+    await db.sync();
     console.log("Connection to the database successful!");
   } catch (error) {
     console.error("Error connecting to the database: ", error);
