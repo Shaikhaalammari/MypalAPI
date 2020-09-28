663daa9b0a70aba06848d5baa9c8b43ff1b31e92
FN Otaibi <nsayemlil@hotmail.com>
2020-09-28 15:39:54 +0300

    moved addMessage from message route to child route

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-//controllers
-const { message } = require("../controllers/messageController");
 
-// Add Child Message
-router.post("/messages", message);
+//controllers
+const { messageList } = require("../controllers/messageController");
+
+// Midleware
+const upload = require("../middleware/multer");
+
+router.param("messageId", async (req, res, next, messageId) => {
+  console.log(`The value of message's ID is ${messageId}`);
+  const message = await fetchMessage(messageId, next);
+  if (message) {
+    req.message = message;
+    next();
+  } else {
+    const err = new Error("Message not found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+// Message List
+router.get("/", messageList);
 
 module.exports = router;
