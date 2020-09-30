4a404b438a3b36bbdfdde3be8472df9294329e7b
Shaikhaalammari <66787565+Shaikhaalammari@users.noreply.github.com>
2020-09-30 19:03:25 +0300

    Fixed Signup

--- db/db.js
+++ db/db.js
@@ -1,15 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize({
+  username: "postgres",
+  password: "shaikha19",
+  database: "Mypal",
+  dialect: "postgres",
+  host: "localhost",
 
-  // username: "postgres",
-  // password: "",
-  // database: "mypal",
-  // dialect: "postgres",
-  // host: "localhost",
-
-  // dialect: "sqlite",
-  // storage: "my_db.db",
   logging: false,
 });
 
